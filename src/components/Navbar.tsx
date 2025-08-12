e613df5f4dcf37106f4998a08ccf100f12df43da
BatuhanKorur <bkorur95@gmail.com>
2025-08-12 14:51:06 +0300

    navbar tooltip removal

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import { useTheme } from 'next-themes'
 import Link from 'next/link'
 import { Dock, DockIcon } from '@/components/magicui/dock'
 import { Separator } from '@/components/ui/separator'
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
 import { SOCIAL_LINKS } from '@/data'
 
 export function Navbar() {
@@ -15,67 +14,32 @@ export function Navbar() {
       className="pointer-events-none fixed inset-x-0 z-50 bottom-[calc(env(safe-area-inset-bottom)+1rem)] flex justify-center"
     >
       <Dock direction="middle" className="pointer-events-auto">
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Link href="/">
-              <DockIcon>
-                <Icon icon="ph:house-bold" className="size-6" />
-              </DockIcon>
-            </Link>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Home</p>
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <DockIcon>
-              <Link href="/cv.pdf" target="_blank" className="font-bold tracking-tighter">
-                CV
-              </Link>
-            </DockIcon>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Resume</p>
-          </TooltipContent>
-        </Tooltip>
+        <DockIcon>
+          <Link href="/">
+            <Icon icon="ph:house-bold" className="size-6" />
+          </Link>
+        </DockIcon>
+        <DockIcon>
+          <Link href="/cv" target="_blank" className="font-bold tracking-tighter">
+            CV
+          </Link>
+        </DockIcon>
         <Separator orientation="vertical" className="h-full" />
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <DockIcon>
-              <Link href={SOCIAL_LINKS.github} target="_blank" className="font-bold tracking-tighter">
-                <Icon icon="mdi:github" className="size-6" />
-              </Link>
-            </DockIcon>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Github</p>
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <DockIcon>
-              <Link href={SOCIAL_LINKS.linkedin} target="_blank" className="font-bold tracking-tighter">
-                <Icon icon="mdi:linkedin" className="size-6" />
-              </Link>
-            </DockIcon>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Linkedin</p>
-          </TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <DockIcon>
-              <Link href={SOCIAL_LINKS.instagram} target="_blank" className="font-bold tracking-tighter">
-                <Icon icon="mdi:instagram" className="size-6" />
-              </Link>
-            </DockIcon>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Instagram</p>
-          </TooltipContent>
-        </Tooltip>
+        <DockIcon>
+          <Link href={SOCIAL_LINKS.github} target="_blank" className="font-bold tracking-tighter">
+            <Icon icon="mdi:github" className="size-6" />
+          </Link>
+        </DockIcon>
+        <DockIcon>
+          <Link href={SOCIAL_LINKS.linkedin} target="_blank" className="font-bold tracking-tighter">
+            <Icon icon="mdi:linkedin" className="size-6" />
+          </Link>
+        </DockIcon>
+        <DockIcon>
+          <Link href={SOCIAL_LINKS.instagram} target="_blank" className="font-bold tracking-tighter">
+            <Icon icon="mdi:instagram" className="size-6" />
+          </Link>
+        </DockIcon>
         <Separator orientation="vertical" className="h-full" />
         <DockIcon onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
           {theme === 'dark'
